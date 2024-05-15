9c404ac0172c52cecf37e8056c58f16e7550414a
agrwldaya <agrawaldaya336@gmail.com>
2024-05-15 17:46:55 +0530

    adding file

---
+++ html/donate.js
@@ -0,0 +1,9 @@
+
+<script>
+        document.addEventListener("DOMContentLoaded", function () {
+            document.getElementById("user-dropdown").addEventListener("click", function () {
+                var dropdownOptions = document.getElementById("dropdown-options");
+                dropdownOptions.classList.toggle("hidden");
+            });
+        });
+    </script>
